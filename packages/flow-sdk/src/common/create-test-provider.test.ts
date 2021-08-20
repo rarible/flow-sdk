a818f87d35811b9ce541020d97399168b94796a2
vitaliyzhalnin <cueage@gmail.com>
2021-08-20 21:10:06 +0700

    add test

--- packages/flow-sdk/src/common/create-test-provider.test.ts
+++ packages/flow-sdk/src/common/create-test-provider.test.ts
@@ -1,24 +1,16 @@
-import path = require("path")
-import { getAccountAddress, init } from "flow-js-testing";
+import { getContractAddress, init } from "flow-js-testing"
 
-const basePath = path.resolve(__dirname, "../cadence");
+const path = require("path")
+
+const basePath = path.resolve(__dirname, "../cadence")
 
 beforeAll(() => {
-	init(basePath);
-});
+	init(basePath)
+})
 
 describe("Accounts", () => {
 	test("Create Accounts", async () => {
-		const Alice = await getAccountAddress("Alice")
-		const Bob = await getAccountAddress("Bob")
-		const Charlie = await getAccountAddress("Charlie")
-		const Dave = await getAccountAddress("Dave")
-
-		console.log("Four accounts were created with following addresses:\n", {
-			Alice,
-			Bob,
-			Charlie,
-			Dave,
-		});
-	});
-});
+		const contract = await getContractAddress("CommonFee")
+		console.log({ contract })
+	})
+})
