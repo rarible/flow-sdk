106d0e0af2879621cbb9a6562caba7bc4038b1c5
vitaliyzhalnin <cueage@gmail.com>
2021-09-02 17:10:46 +0700

    fix flow interface

--- packages/flow-sdk/src/temp.ts
+++ packages/flow-sdk/src/temp.ts
@@ -1,42 +1,4 @@
-import { Address, BigNumber } from "@rarible/types"
-
-type Part = {
-	account: Address,
-	value: number
-}
-
-type BlockchainType = "ETH" | "FLOW"
-
-type TokenId = number
-
-type FlowCollectionAddress = string //todo "A.${FlowAddress}.${string}"
-
-type NftItemId = string // todo "FLOW:${FlowAddress}:${tokenId}" FLOW must be ${blockchain}
-
-type Collection = FlowCollectionAddress
-
-type FlowOrderType = "RARIBLE_FLOW_V1"
-
-type FlowAssetType = {
-	assetClass: "FLOW_NFT"
-	contract: FlowAddress
-	tokenId: BigNumber
-}
-
-/**
- * NFT
- */
-type MintRequest = {
-	collection: FlowCollectionAddress
-	url: string
-	royalties: Part[]
-}
-
-type TransferRequest = {
-	blockchain: BlockchainType
-	tokenId: TokenId
-	to: FlowAddress
-}
+import { BigNumber } from "@rarible/types"
 
 type RaribleNftSdk = {
 	mint(data: MintRequest): Promise<TokenId>,
@@ -44,10 +6,27 @@ type RaribleNftSdk = {
 	burn(tokenId: TokenId): Promise<string>
 }
 
+type RaribleOrderSdk = {
+	sell(data: SellRequest): Promise<string>
+	fill(data: FillRequest): Promise<string>
+}
+
+type RaribleSdk = {
+	nft: RaribleNftSdk
+	order: RaribleOrderSdk
+}
+
 async function mint(data: MintRequest): Promise<TokenId> {
 	const mintRequestToPassIntoTransaction = {
 		metadata: data.url,
 		royalties: data.royalties,
+	}
+	return Promise.resolve(123)
+}
+
+async function butn(tokenId: TokenId): Promise<TokenId> {
+	const mintRequestToPassIntoTransaction = {
+		tokenId,
 	}
 	return Promise.resolve(123)
 }
@@ -60,30 +39,9 @@ async function transfer(data: TransferRequest): Promise<string> {
 	return Promise.resolve("hash")
 }
 
-/**
- * Order
- */
-type SellRequest = {
-	asset: FlowAssetType
-	price: BigNumber
-}
-
-
-// buy
-type FillRequest = {
-	asset: FlowAssetType
-	owner: FlowAddress
-	orderId: number
-}
-
-type RaribleOrderSdk = {
-	sell(data: SellRequest): Promise<string>
-	fill(data: FillRequest): Promise<string>
-}
-
 async function sell(data: SellRequest): Promise<string> {
 	const sellRequestToSendTx = {
-		saleItemID: data.asset.tokenId,
+		saleItemID: data.itemId,
 		saleItemPrice: data.price,
 	}
 	return Promise.resolve("hash")
@@ -97,61 +55,102 @@ async function fill(data: FillRequest): Promise<string> {
 	return Promise.resolve("hash")
 }
 
+type TokenId = number
+
+type FlowCollectionAddress = string //todo "A.${FlowAddress}.${string}"
+
+/**
+ * NFT requests
+ */
+type MintRequest = {
+	collection: FlowCollectionAddress
+	url: string
+	royalties: PayInfo[]
+}
+
+type TransferRequest = {
+	tokenId: TokenId
+	to: FlowAddress
+}
+
+/**
+ * Order requests
+ */
+type SellRequest = {
+	itemId: string
+	price: BigNumber
+}
+
+
+// buy
+type FillRequest = {
+	owner: FlowAddress
+	orderId: number
+}
+
+
 /**
  * NftItem
  */
-type NftItem = {
-	"blockchain": BlockchainType,
-	"id": NftItemId,
-	"tokenId": TokenId,
-	"collection": Collection,
-	"creators": Part[],
-	"owners": FlowAddress[],
-	"royalties": Part[],
-	"mintedAt": "2019-08-24T14:15:22Z",
-	"lastUpdatedAt": "2019-08-24T14:15:22Z",
-	"supply": BigNumber,
-	"metaURL": string,
-	"meta": {
-		"name": "string",
-		"description": "string",
-		"attributes": [],
-		"contents": [],
-		"raw": "string"
-	},
-	"deleted": true
+interface FlowNftItem {
+	id?: string;
+	contract?: string;
+	tokenId?: number;
+	creator?: string;
+	owner?: string;
+	meta?: string;
+	date?: Date;
+	listed?: boolean;
+	collection?: string;
 }
+
 /**
  * Order
  */
-type FlowOrder = {
-	"id": 717802,
-	"type": FlowOrderType,
-	"maker": FlowAddress,
-	"taker": FlowAddress,
-	"make": {
-		"assetType": FlowAssetType,
-		"value": 717802
-	},
-	"take": {
-		"assetType": FlowAssetType,
-		"value": 717802
-	},
-	"data": {
-		"dataType": "DATA_V1",
-		"payouts": [],
-		"originFees": []
-	},
-	"fill": 717802,
-	"startedAt": "2019-08-24T14:15:22Z",
-	"endedAt": "2019-08-24T14:15:22Z",
-	"makeStock": 717802.342336,
-	"cancelled": true,
-	"createdAt": "2019-08-24T14:15:22Z",
-	"lastUpdatedAt": "2019-08-24T14:15:22Z",
-	"makeBalance": 717802.342336,
-	"makePriceUSD": 717802.342336,
-	"takePriceUSD": 717802.342336
+export interface PayInfo {
+	account: string;
+	value: string;
+}
+
+export interface FlowOrderData {
+	payouts: Array<PayInfo>;
+	originalFees: Array<PayInfo>;
+}
+
+export interface FlowAssetNFT {
+	type: FlowAssetNFTTypeEnum;
+	tokenId: string;
+}
+
+export enum FlowAssetNFTTypeEnum {
+	Nft = "nft"
+}
+
+export interface FlowAssetFungible {
+	type: FlowAssetFungibleTypeEnum;
+}
+
+export enum FlowAssetFungibleTypeEnum {
+	Fungible = "fungible"
+}
+
+type FlowAsset = FlowAssetFungible | FlowAssetNFT;
+
+export interface FlowOrder {
+	itemId: string;
+	collection: string;
+	maker: string;
+	taker?: string;
+	make: FlowAsset;
+	take?: FlowAsset;
+	data: FlowOrderData;
+	fill: string;
+	cancelled: boolean;
+	createdAt: Date;
+	lastUpdateAt: Date;
+	amount: string;
+	offeredNftId?: string;
+	amountUsd: string;
 }
 
 declare const validAddress: unique symbol
