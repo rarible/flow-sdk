9381f01a6e7bd4585f49441ae5e83dd7b560fea3
vitaliyzhalnin <cueage@gmail.com>
2021-09-23 13:12:10 +0300

    added cancel order func

--- packages/flow-sdk/src/index.ts
+++ packages/flow-sdk/src/index.ts
@@ -5,6 +5,7 @@ import { burn as burnTemplate } from "./nft/burn"
 import { transfer as transferTemplate } from "./nft/transfer"
 import { sell as sellTemplate } from "./order/sell"
 import { buy as buyTemplate } from "./order/buy"
+import { cancelOrder as cancelOrderTmeplate } from "./order/cancel-order"
 import { CONFIGS, Networks } from "./config"
 import { signUserMessage } from "./utils/sign-user-message"
 
@@ -17,6 +18,7 @@ export type FlowNftSdk = {
 export type FlowOrderSdk = {
 	sell(collection: string, sellItemId: number, sellItemPrice: string): Promise<string>
 	buy(collection: string, itemId: number, owner: string): Promise<string>
+	cancelOrder(collection: string, orderId: number): Promise<string>
 }
 
 type FlowSdk = {
@@ -28,12 +30,15 @@ type FlowSdk = {
 export function createFlowSdk(network: Networks): FlowSdk {
 	config()
 		.put("accessNode.api", CONFIGS[network].accessNode)
-		.put("challenge.handshake", "https://fcl-discovery.onflow.org/testnet/authn")
+		.put("challenge.handshake", "https://fcl-discovery.onflow.org/testnet/authn")// todo set based on network value
+
 	const mint = mintTemplate.bind(null, network)
 	const transfer = transferTemplate.bind(null, network)
 	const burn = burnTemplate.bind(null, network)
+
 	const sell = sellTemplate.bind(null, network)
 	const buy = buyTemplate.bind(null, network)
+	const cancelOrder = cancelOrderTmeplate.bind(null, network)
 
 	return {
 		nft: {
@@ -44,6 +49,7 @@ export function createFlowSdk(network: Networks): FlowSdk {
 		order: {
 			sell,
 			buy,
+			cancelOrder,
 		},
 		signUserMessage,
 	}
--- packages/flow-sdk/src/order/cancel-order.ts
+++ packages/flow-sdk/src/order/cancel-order.ts
@@ -1,12 +1,12 @@
 import { CommonNftOrder, runTransaction, waitForSeal } from "@rarible/flow-sdk-scripts"
 import { CONFIGS, Networks } from "../config"
 
-export async function buy(network: Networks, collection: string, orderId: number, owner: string) {
+export async function cancelOrder(network: Networks, collection: string, orderId: number) {
 	const collectionAddress = CONFIGS[network].contracts.CommonNFT
 	const addressMap = CONFIGS[network].contracts
 	switch (collection) {
 		case `A.${collectionAddress}.CommonNFT.NFT`: {
-			const txId = await runTransaction(network, addressMap, CommonNftOrder.buyItem(orderId, owner))
+			const txId = await runTransaction(network, addressMap, CommonNftOrder.removeItem(orderId))
 			await waitForSeal(txId)
 			return txId
 		}
