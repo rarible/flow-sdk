4aac779c83a7640b4048730c0240e94dd6155cf6
vitaliyzhalnin <cueage@gmail.com>
2021-10-04 10:10:32 +0300

    remove unused code

--- packages/flow-sdk-scripts/src/common.ts
+++ packages/flow-sdk-scripts/src/common.ts
@@ -1,22 +1,3 @@
-// import * as fcl from "@onflow/fcl"
-//
-// export const runScript = async (script: any[]) =>
-// 	await fcl.send(script).then(fcl.decode)
-//
-// export const runTransaction = async (tx: any[]) =>
-// 	await fcl.send([
-// 		...tx,
-// 		fcl.payer(fcl.authz),
-// 		fcl.proposer(fcl.authz),
-// 		fcl.authorizations([fcl.authz]),
-// 		fcl.limit(9999),
-// 	]).then(fcl.decode)
-//
-// export const waitForSeal = async (txId: string) =>
-// 	await fcl.tx(txId).onceSealed()
-//
-// export const contractAddressHex = async (label: string) =>
-// 	fcl.sansPrefix(await fcl.config.get(label))
 import * as fcl from "@onflow/fcl"
 import { authorization } from "./crypto"
 import { replaceImportAddresses } from "./utils/replace-imports"
