ef99b864a2bf5725ed42baf29faab6ef6505cd47
vzhalnin <cueage@yandex.ru>
2021-10-26 19:56:29 +0300

    fix filling transaction by contract addresses

--- packages/flow-sdk/src/common/utils.ts
+++ packages/flow-sdk/src/common/utils.ts
@@ -2,3 +2,8 @@ export const sansPrefix = (address: string): string | null => {
 	if (address == null) return null
 	return address.replace(/^0x/, "")
 }
+
+export function withPrefix(address: string): string | null {
+	if (address == null) return null
+	return "0x" + sansPrefix(address)
+}
