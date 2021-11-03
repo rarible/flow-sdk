5af1fcf70cfa2b5d6e7b672ed4fbe140e8b613b2
vzhalnin <cueage@yandex.ru>
2021-11-03 17:06:12 +0300

    remove extra code

--- packages/flow-sdk-scripts/src/wallet/balance.ts
+++ packages/flow-sdk-scripts/src/wallet/balance.ts
@@ -10,10 +10,6 @@ export const getBalanceScripts = {
 	fusd: `import FungibleToken from 0xf233dcee88fe0abe
 import FUSD from 0x3c5959b568896393
 
-// Testnet
-// import FungibleToken from 0x9a0766d93b6608b7
-// import FUSD from 0xe223d8a629e49c68
-
 pub fun main(address: Address): UFix64 {
   let account = getAccount(address)
 
