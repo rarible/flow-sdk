a8d79b25574524b2a31b25161cc687e803e5b173
Ivan Koryakovtsev <ivan@rarible.com>
2021-11-04 18:41:35 +0300

    feat: improve fcl types

--- packages/flow-sdk/src/signature/sign-user-message.ts
+++ packages/flow-sdk/src/signature/sign-user-message.ts
@@ -1,18 +1,20 @@
-import { Fcl } from "@rarible/fcl-types"
+import type { Fcl } from "@rarible/fcl-types"
 
 export async function signUserMessage(fcl: Fcl, message: string): Promise<string> {
 	const currentUser = fcl.currentUser()
-	const userAddress = (await currentUser.snapshot()).addr
+	const snapshot = await currentUser.snapshot()
+	const address = snapshot.addr
+	if (!address) {
+		throw new Error("Unauthorized request")
+	}
 	const messageHex = Buffer.from(message).toString("hex")
 	const signatures = await currentUser.signUserMessage(messageHex)
 	if (signatures.length) {
-		const signature = signatures.find(s => s.addr.toLowerCase() === userAddress.toLowerCase())?.signature
+		const signature = signatures.find(s => s.addr.toLowerCase() === address.toLowerCase())?.signature
 		if (signature) {
 			return signature
-		} else {
-			throw Error(`Signature of user address "${userAddress}" not found`)
 		}
-	} else {
-		throw Error("Response of signUserMessage is empty")
+		throw Error(`Signature of user address "${address}" not found`)
 	}
+	throw Error("Response of signUserMessage is empty")
 }
