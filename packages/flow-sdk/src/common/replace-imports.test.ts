26d9dd93357cb6958dfda916c2f8c112fb3981d2
Ivan Koryakovtsev <ivan@rarible.com>
2021-11-04 18:43:22 +0300

    chore: remove unecessary escapes

--- packages/flow-sdk/src/common/replace-imports.test.ts
+++ packages/flow-sdk/src/common/replace-imports.test.ts
@@ -13,7 +13,7 @@ describe("replace import", () => {
 			{
 				Contract1Name: "0x01",
 				Contract2Name: "0x01",
-			},
+			}
 		)
 		expect(replaceResult).toBe(`
 			import Contract1Name from 0x01
@@ -23,23 +23,23 @@ describe("replace import", () => {
 	})
 	test("should add topShot fee mainnet", () => {
 		const result1 = replaceImportAddresses(StorefrontTopShot.sell_flow, CONFIGS.mainnet.mainAddressMap)
-		expect(result1.search(/PaymentPart\(address\: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
+		expect(result1.search(/PaymentPart\(address: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
 		const result2 = replaceImportAddresses(StorefrontTopShot.sell_fusd, CONFIGS.mainnet.mainAddressMap)
-		expect(result2.search(/PaymentPart\(address\: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
+		expect(result2.search(/PaymentPart\(address: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
 		const result3 = replaceImportAddresses(StorefrontTopShot.update_flow, CONFIGS.mainnet.mainAddressMap)
-		expect(result3.search(/PaymentPart\(address\: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
+		expect(result3.search(/PaymentPart\(address: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
 		const result4 = replaceImportAddresses(StorefrontTopShot.update_fusd, CONFIGS.mainnet.mainAddressMap)
-		expect(result4.search(/PaymentPart\(address\: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
+		expect(result4.search(/PaymentPart\(address: 0xbd69b6abdfcf4539/)).toBeGreaterThan(0)
 	})
 
 	test("should add topShot fee testnet", () => {
 		const result1 = replaceImportAddresses(StorefrontTopShot.sell_flow, CONFIGS.testnet.mainAddressMap)
-		expect(result1.search(/PaymentPart\(address\: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
+		expect(result1.search(/PaymentPart\(address: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
 		const result2 = replaceImportAddresses(StorefrontTopShot.sell_fusd, CONFIGS.testnet.mainAddressMap)
-		expect(result2.search(/PaymentPart\(address\: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
+		expect(result2.search(/PaymentPart\(address: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
 		const result3 = replaceImportAddresses(StorefrontTopShot.update_flow, CONFIGS.testnet.mainAddressMap)
-		expect(result3.search(/PaymentPart\(address\: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
+		expect(result3.search(/PaymentPart\(address: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
 		const result4 = replaceImportAddresses(StorefrontTopShot.update_fusd, CONFIGS.testnet.mainAddressMap)
-		expect(result4.search(/PaymentPart\(address\: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
+		expect(result4.search(/PaymentPart\(address: 0xebf4ae01d1284af8/)).toBeGreaterThan(0)
 	})
 })
