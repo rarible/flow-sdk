dca2d83fc6bd4715cc881f1d64006e6f08c0f22c
Ivan Koryakovtsev <ivan@rarible.com>
2021-11-05 21:03:44 +0300

    chore: remove redunant dependencies

--- packages/flow-sdk/src/typings.d.ts
+++ packages/flow-sdk/src/typings.d.ts
@@ -1,29 +1,17 @@
-declare module "@onflow/config"
-declare module "@testing-library/dom"
-declare module "cwd"
 declare module "@onflow/fcl"
 declare module "@onflow/types"
 
 declare module "flow-js-testing" {
 	export function init(basePath: string): Promise<void>
 	export function init(basePath: string, props: object): Promise<void>
-
 	export function mintFlow(recipient: string, amount: string): Promise<any>
-
 	export function shallPass(ix: Function | Promise<any>): Promise<any>
-
 	export function sendTransaction(props: object): Promise<any>
-
 	export function executeScript(props: object): Promise<any>
-
 	export function getContractAddress(name: string): Promise<string>
 	export function getContractAddress(name: string, useDefaults: boolean): Promise<string>
-
 	export function deployContractByName(props: object): Promise<any>
-
 	export const emulator: any
-
 	export function getAccountAddress(accountName: string): Promise<string>
-
 	export function script(...args: any[]): any
 }
