6f66d523c6766b7190f21ecd49d282891330e109
vzhalnin <cueage@yandex.ru>
2021-11-06 10:23:29 +0300

    fix: support burn/transfer fro secondary collections

--- packages/flow-sdk/src/nft/burn.ts
+++ packages/flow-sdk/src/nft/burn.ts
@@ -1,8 +1,7 @@
 import type { Fcl } from "@rarible/fcl-types"
-import type { FlowNetwork } from "../types"
+import type { AuthWithPrivateKey, FlowNetwork } from "../types"
 import { runTransaction, waitForSeal } from "../common/transaction"
 import { getNftCode } from "../tx-code-store/ntf"
-import type { AuthWithPrivateKey } from "../types"
 import type { FlowContractAddress } from "../common/flow-address"
 import { getCollectionConfig } from "../common/collection/get-config"
 
@@ -11,13 +10,10 @@ export async function burn(
 	auth: AuthWithPrivateKey,
 	network: FlowNetwork,
 	collection: FlowContractAddress,
-	tokenId: number
+	tokenId: number,
 ) {
-	const { map, config, name } = getCollectionConfig(network, collection)
-	if (config.mintable) {
-		const txId = await runTransaction(fcl, map, getNftCode(name).burn(fcl, tokenId), auth)
-		return await waitForSeal(fcl, txId)
-	} else {
-		throw new Error("This collection doesn't support 'burn'")
-	}
+	const { map, name } = getCollectionConfig(network, collection)
+	const txId = await runTransaction(fcl, map, getNftCode(name).burn(fcl, tokenId), auth)
+	return await waitForSeal(fcl, txId)
+
 }
