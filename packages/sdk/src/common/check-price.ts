1cb781ebb1f8907aabf76236ff10e3451a2d4605
vzhalnin <cueage@yandex.ru>
2021-11-16 14:30:40 +0700

    fix: move min-price variable to config

--- packages/sdk/src/common/check-price.ts
+++ packages/sdk/src/common/check-price.ts
@@ -1,11 +1,10 @@
 import { toBn } from "@rarible/utils"
+import { MIN_ORDER_PRICE } from "../config"
 import { fixAmount } from "./fix-amount"
-
-const MIN_PRICE = "0.0001"
 
 export function checkPrice(price: string): void {
 	const fixedPrice = fixAmount(price)
-	if (toBn(fixedPrice).lt(toBn(MIN_PRICE))) {
+	if (toBn(fixedPrice).lt(toBn(MIN_ORDER_PRICE))) {
 		throw new Error(`Invalid price, minimal value is ${0.0001}`)
 	}
 }
