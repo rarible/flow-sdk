c5adcf01dc5d4ec4465161b44306476ad4fc3137
vzhalnin <cueage@yandex.ru>
2022-01-21 11:32:31 +0700

    fix: fee test

--- packages/sdk/src/order/get-protocol-fee.test.ts
+++ packages/sdk/src/order/get-protocol-fee.test.ts
@@ -1,8 +1,6 @@
 import { createEmulatorAccount, createFlowEmulator, createTestAuth } from "@rarible/flow-test-common"
 import fcl from "@onflow/fcl"
-import { toBn } from "@rarible/utils"
 import { createFlowSdk } from "../index"
-import { CONFIGS } from "../config/config"
 import { getProtocolFee } from "./get-protocol-fee"
 
 describe("Get protocol fee", () => {
@@ -11,17 +9,16 @@ describe("Get protocol fee", () => {
 		const { address, pk } = await createEmulatorAccount("accountName1")
 		const auth = createTestAuth(fcl, "emulator", address, pk, 0)
 		const sdk = createFlowSdk(fcl, "emulator", {}, auth)
-		const fee = CONFIGS["emulator"].protocolFee.value
 		const fees = sdk.order.getProtocolFee()
-		expect(fees.buyerFee.value).toEqual(fee)
+		expect(fees.buyerFee.value).toEqual("0")
 		expect(fees.buyerFee.account).toBeTruthy()
-		expect(fees.sellerFee.value).toEqual(fee)
+		expect(fees.sellerFee.value).toEqual("0")
 		expect(fees.sellerFee.account).toBeTruthy()
 
 		const percentFees = getProtocolFee.percents("testnet")
-		expect(percentFees.buyerFee.value).toEqual(toBn(fee).dividedBy(10000).toString())
+		expect(percentFees.buyerFee.value).toEqual("0")
 		expect(percentFees.buyerFee.account).toEqual("0xebf4ae01d1284af8")
-		expect(percentFees.sellerFee.value).toEqual(toBn(fee).dividedBy(10000).toString())
+		expect(percentFees.sellerFee.value).toEqual("0")
 		expect(percentFees.sellerFee.account).toEqual("0xebf4ae01d1284af8")
 	})
 })
