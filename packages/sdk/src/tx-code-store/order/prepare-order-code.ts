38d94d992304717143396e8f23d43cf9dbf1ca70
vzhalnin <cueage@yandex.ru>
2022-02-03 18:25:12 +0700

    fix: prepare order code function

--- packages/sdk/src/tx-code-store/order/prepare-order-code.ts
+++ packages/sdk/src/tx-code-store/order/prepare-order-code.ts
@@ -1,5 +1,5 @@
-import type { FlowContractName, FlowCurrency, NonFungibleContract } from "../../types"
-import type { FtCodeConfig, NftCodeConfig } from "../../config/cadence-code-config"
+import type { FlowCurrency, NonFungibleContract } from "../../types"
+import type { FtCodeConfig } from "../../config/cadence-code-config"
 import { getFtCodeConfig, getNftCodeConfig } from "../../config/cadence-code-config"
 import { fillCodeTemplate } from "../../common/template-replacer"
 
@@ -15,19 +15,9 @@ export function prepareOrderCode(code: string, collectionName: NonFungibleContra
 		default:
 			throw new Error("Unsupported currency")
 	}
-	let nftCodeData: NftCodeConfig
-	switch (collectionName) {
-		case "RaribleNFT":
-		case "TopShot":
-		case "MotoGPCard":
-		case "Evolution":
-		case "MugenNFT":
-		case "CNN_NFT":
-			nftCodeData = getNftCodeConfig(collectionName as FlowContractName)
-			break
-		default:
-			throw new Error("Unsupported collection")
-	}
-	const codeWithFt = fillCodeTemplate(code, ftData)
-	return fillCodeTemplate(codeWithFt, nftCodeData)
+
+	return fillCodeTemplate(
+		fillCodeTemplate(code, ftData),
+		getNftCodeConfig(collectionName),
+	)
 }
