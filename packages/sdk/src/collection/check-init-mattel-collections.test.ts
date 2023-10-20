14609c8232446967ab22f3ae7dec4af880d2daf6
Cristian Covali <101266536+chris-covali@users.noreply.github.com>
2023-10-20 12:44:50 +0300

    feat(PLTF-43): check collection init for different address than current (#106)

--- packages/sdk/src/collection/check-init-mattel-collections.test.ts
+++ packages/sdk/src/collection/check-init-mattel-collections.test.ts
@@ -3,7 +3,8 @@ import {
 	createTestAuth,
 } from "@rarible/flow-test-common"
 import * as fcl from "@onflow/fcl"
-import { FLOW_TESTNET_ACCOUNT_5 } from "@rarible/flow-test-common/build/config"
+import { FLOW_TESTNET_ACCOUNT_1, FLOW_TESTNET_ACCOUNT_5 } from "@rarible/flow-test-common/build/config"
+import { toFlowAddress } from "@rarible/types"
 import { createFlowSdk } from "../index"
 
 describe("Check init mattel collections", () => {
@@ -27,4 +28,32 @@ describe("Check init mattel collections", () => {
 			expect(key in status).toBeTruthy()
 		})
 	}, 1000000)
+
+	test("collections keys exists and are false for a different wallet", async () => {
+		const testnetBuyerAuth = createTestAuth(
+			fcl,
+			"testnet",
+			FLOW_TESTNET_ACCOUNT_5.address,
+			FLOW_TESTNET_ACCOUNT_5.privKey
+		)
+		const testnetBuyerSdk = createFlowSdk(fcl, "testnet", {}, testnetBuyerAuth)
+
+		const address = toFlowAddress(FLOW_TESTNET_ACCOUNT_1.address)
+		const status = await testnetBuyerSdk.collection.checkInitMattelCollections(address);
+		(
+			[
+				"FiatToken",
+				"StorefrontV2",
+				"HWGarageCard",
+				"HWGarageCardV2",
+				"HWGaragePack",
+				"HWGaragePackV2",
+				"BBxBarbieCard",
+				"BBxBarbiePack",
+			] as Array<keyof typeof status>
+		).forEach(key => {
+			expect(key in status).toBeTruthy()
+			expect(status[key]).toBeFalsy()
+		})
+	})
 })
